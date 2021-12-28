da26542c745b67b0ba3b43d5839d0d9f6a322ad4
mancongchua <96785459+mancongchua@users.noreply.github.com>
2021-12-28 23:12:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'chào anh iu',
+    introDesc: `em có điều này muốn hỏi anh, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    title: 'anh yêu em không',
+    desc: 'giả bộ bấm không đi có cái này dui lúm :))',
     btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnNo: 'không',
+    question:'Nói cho tới lý do vì sao anh yêu em đii <3',
+    btnReply: 'moa moa',
+    reply: 'Tại vì cậu đáng iu hihi <33333333',
+    mess: ' em pít mà. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không nhắn tin cho em đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100031788195396' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
